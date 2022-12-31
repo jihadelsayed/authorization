4e8ad7eeba75451a6180651e794002a8268e5f09
jihad <david.pales1@gmail.com>
2022-12-31 14:28:05 +0100

    fdg

--- authorization.component.ts
+++ authorization.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-authorization',
@@ -8,8 +9,31 @@ import { Component, OnInit } from '@angular/core';
 export class AuthorizationComponent implements OnInit {
 
   constructor() { }
+  LoginURL = environment.LoginURL;
+  frameSrc:any
+  ngOnInit() {
+    // Authorization
+    if (localStorage.getItem("userToken") == null) {
+      const topWindow:any = window.top;
+      topWindow.addEventListener("message", (event:any) => {
+        if (localStorage.getItem("userToken") == null) {
+          if (event.origin === "http://localhost:4200/" || event.origin === "https://theislamicnation.web.app/"|| event.origin === "https://theislamicnation.com/") {
+            return;
+          } else {
+            if (event.data['type'] === "credential") {
+              if (event.data.getToken !== 'undefined') {
+                localStorage.setItem("userToken", event.data.getToken)
+                localStorage.setItem("UserInfo", event.data.getUserInfo)
+              }
+            }
+          }
+        }
+      }, false);
+    }
 
-  ngOnInit(): void {
+    this.frameSrc= this.LoginURL+ window.navigator.language.substring(0, 2)+"/#/getCredential"+"?"+ "host="+ window.location.href+"&"+"language="+ window.navigator.language +"&" + "pathname="+window.location.pathname;
+    (document.getElementById('iframeAccount')as any)["src"] = this.frameSrc;
+    console.log(this.frameSrc)
   }
 
 }
