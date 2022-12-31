25be47c8d29f90eb0c7312004c8ed29881fe3750
jihad <david.pales1@gmail.com>
2022-12-31 20:51:45 +0100

    working in sha allah

--- authorization.component.ts
+++ authorization.component.ts
@@ -9,30 +9,36 @@ import { environment } from 'src/environments/environment';
 export class AuthorizationComponent implements OnInit {
 
   constructor() { }
-  frameSrc = environment.LoginURL + window.navigator.language.substring(0, 2)+"/#/getCredential";
+  frameSrc = environment.LoginURL + window.navigator.language.substring(0, 2)+"/#/getCredential"+"?"+ "host="+ window.location.href+"&"+"language="+ window.navigator.language +"&" + "pathname="+window.location.pathname;
   ngOnInit() {
+    console.log("va"+this.frameSrc);
+    (document.getElementById('iframeAccount')as any)["src"] = this.frameSrc;
+
     // Authorization
-    if (localStorage.getItem("userToken") == null) {
+    if (localStorage.getItem("userToken") || null == null) {
       const topWindow:any = window.top;
       topWindow.addEventListener("message", (event:any) => {
-        if (localStorage.getItem("userToken") == null) {
-          // if (event.origin === "http://localhost:4200/"
-          // || event.origin === "https://theislamicnation.web.app/"
-          // || event.origin === "https://theislamicnation.com/") {
-          //   return;
-          // } else {
-          //   if (event.data['type'] === "credential") {
-          //     if (event.data.getToken !== 'undefined') {
+        if (localStorage.getItem("userToken") || null == null) {
+
+          if (event.origin ===  window.location.protocol + '//' + window.location.host) {
+            return;
+          } else {
+            //console.log( window.location.protocol + '//' + window.location.host)
+            console.log(event)
+
+            if (event.data['type'] === "credential") {
+              if (event.data.getToken !== 'undefined') {
                 localStorage.setItem("userToken", event.data.getToken)
                 localStorage.setItem("UserInfo", event.data.getUserInfo)
-          //     }
-          //   }
-          // }
+              }
+            }
+          }
         }
       }, false);
     }
+    (document.getElementById('iframeAccount')as any)["src"] = this.frameSrc;
 
-    // (document.getElementById('iframeAccount')as any)["src"] = this.frameSrc;
+    console.log(this.frameSrc)
   }
 
 }
